9f1547bba1c75f8da591d1ad44e3c8a628a8cfda
tolasom <somtola16@kit.edu.kh>
2018-05-11 01:00:26 +0700

    add page-scope id

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+// const PAGE_ACCESS_TOKEN= process.env.PAGE_ACCESS_TOKEN;
 var Config = require('./config');
 var FbHubVerify = require('./controllers/verification');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.get('/',FbHubVerify);
-app.get('/privacy', function(req, res, next) {
+app.get('/privacy', (req, res, next)=> {
 	res.status(200).send(`
 		My privacy is so simple :D
 	`);
 });
+app.post('/webhook',(req,res)=>{
 
+	let body = req.body;
+	if(body.object==='page'){
+		body.entry.forEach(function(entry){
+			let webhook_event= entry.message[0];
+			console.log(webhook_event);
+			//get sender page-scope id
 
+			let sender_psid=webhook_event.sender_psid;
+			console.log('Page-Scope ID'+sender_psid);
+		});
+		app.status(200).send(`Event_Recieved`);
+	}else{
+		app.status(404);
+	}
+});
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use('', FbHubVerify);
-// app.use('privacy', function(req, res, next) {
-// 	res.status(200).send(`
-// 		My privacy is so simple :D
-// 	`);
+// app.get('/Webhook',(req,res)=>{
+// 	const VERIFY_TOKEN = "verify_token";
+// 	res.status(200).send(`Webhook is running`);
 // });
+
 app.listen(Config.PORT,() => console.log('Webhook server is listening on port '+Config.PORT));
 
 
