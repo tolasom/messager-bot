745ce905849ecfa5e92ba231db41b114d2afa684
tolasom <somtola16@kit.edu.kh>
2018-05-11 10:33:20 +0700

    add webhook

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 function handleMessage(sender_psid,recieved_message){}
 function handlePostback(sender_psid,recieved_postback){}
 function callSendAPI(sender_psid,response){}
-app.get('/webhook',(req,res,next)=>{
+app.post('/webhook',(req,res,next)=>{
 	let body = req.body;
 	if(body.object==='page'){
 		body.entry.forEach(function(entry){
